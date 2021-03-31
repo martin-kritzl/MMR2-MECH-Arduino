56a2025ebda5e0d60eee03fc376ca34d2aea1494
Martin Kritzl <github@martin.kritzl.at>
2021-03-31 12:39:50 +0200

    added interface description, example function calls and structs for move commands

--- MMR2-MECH-Arduino.h
+++ MMR2-MECH-Arduino.h
@@ -2,3 +2,26 @@
 #include <SoftwareSerial.h>
 
 #define BUFFER_LEN 20
+#define RX_PIN 10
+#define TX_PIN 11
+
+/** 
+ * @struct DriveInstruction
+ * @brief Struct which contains the drive instruction (absolute angle and speed) of a motor.
+ */
+struct DriveInstruction
+{
+    int angle; ///< Absolute angle of the motor [degrees].
+    int speed=-1; ///< Speed of the motor [rpm].
+};
+
+/** 
+ * @struct RobotInstruction
+ * @brief Struct which contains the move commands for all three axes.
+ */
+struct RobotInstruction
+{
+    DriveInstruction motor1; ///< Drive command of the first motor
+    DriveInstruction motor2; ///< Drive command of the second motor
+    DriveInstruction motor3; ///< Drive command of the third motor
+};
--- MMR2-MECH-Arduino.ino
+++ MMR2-MECH-Arduino.ino
@@ -1,8 +1,41 @@
 #include "MMR2-MECH-Arduino.h"
 
+MSmartServo mysmartservo(RX_PIN,TX_PIN);
+DriveInstruction move_buffer[BUFFER_LEN];
+
+/**
+ * Interface:
+ *      incoming: move;<theta1>;<speed1>;<theta2>;<speed2>;<theta3>;<speed3>
+ *      result: done;move;<theta1>;<speed1>;<theta2>;<speed2>;<theta3>;<speed3>
+ * 
+ *      incoming: move;home
+ *      result: done
+ * 
+ *      incoming: move;stop
+ * 
+ *      incoming: read;angles
+ *      result: <theta1>;<theta2>;<theta3>
+ * 
+ *      incoming: read;status
+ *      result: [idle,moving]
+ * 
+ *      incoming: set;init
+ *      result: done
+ */
+
 void setup() {
+    mysmartservo.begin(115200);         // set the data rate for the Smart Servos
+    delay(5);
+    Serial.begin(115200);               // set the data rate for the SoftwareSerial port
+    mysmartservo.assignDevIdRequest();  // distribution device ID number to the smart servo link.
+    delay(50);                          // must delay over 50ms
 }
 
 void loop() {
-    
+    //Important commands:
+    //mysmartservo.setInitAngle(<id>); //This function used to get the smart servo's angle.
+    //mysmartservo.setZero(<id>); //set smart servo current angle zero postion.
+    //mysmartservo.moveTo(<id>,<angle [degrees]>,<speed [rpm]>); //smart servo moves to the absolute angle.
+    //mysmartservo.setBreak(<id>;<release [true,false]>); //set smart servo break status.
+    //mysmartservo.getAngleRequest(<id>); //This function used to get the smart servo's angle.
 }
